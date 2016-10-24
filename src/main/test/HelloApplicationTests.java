50e0ebb3fb6467be7ccb30e051b26bc92bf90049
lux182 <562182429@qq.com>
2016-10-24 11:07:35 +0800

    Initial commit

---
+++ src/main/test/HelloApplicationTests.java
@@ -0,0 +1,18 @@
+import mq.HelloApplication;
+import mq.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloApplication.class)
+public class HelloApplicationTests {
+    @Autowired
+    private Sender sender;
+    @Test
+    public void hello() throws Exception {
+        sender.send();
+    }
+}
